81882f921ac446fb778ccebeed7ffd6c0f2c1a9e
Paolo Galeone <nessuno@nerdz.eu>
2016-11-23 15:53:07 +0100

    exec.sh: train.py -> train_classificator.py

--- exec.sh
+++ exec.sh
@@ -5,43 +5,43 @@
 ########################################################
 
 # VGG: no regularization
-python train.py --model vgg --dataset cifar10
+python train_classificator.py --model vgg --dataset cifar10
 
 # VGG: L2 (every layer)
-python train.py --model vgg --dataset cifar10 --l2_penalty "5e-4"
+python train_classificator.py --model vgg --dataset cifar10 --l2_penalty "5e-4"
 
 # VGG: BN (every layer)
-python train.py --model vgg_bn --dataset cifar10
+python train_classificator.py --model vgg_bn --dataset cifar10
 
 # VGG: dropout (every layer)
-python train.py --model vgg_dropout --dataset cifar10
+python train_classificator.py --model vgg_dropout --dataset cifar10
 
 # VGG: binomial dropout (every layer)
-python train.py --model vgg_binomial_dropout --dataset cifar10
+python train_classificator.py --model vgg_binomial_dropout --dataset cifar10
 
 # VGG: direct dropout (every layer)
-python train.py --model vgg_direct_dropout --dataset cifar10
+python train_classificator.py --model vgg_direct_dropout --dataset cifar10
 
 # VGG: direct binomial dropout (every layer)
-python train.py --model vgg_direct_binomial_dropout --dataset cifar10
+python train_classificator.py --model vgg_direct_binomial_dropout --dataset cifar10
 
 # LeNet: no regularization
-python train.py --model lenet --dataset mnist
+python train_classificator.py --model lenet --dataset mnist
 
 # LeNet: L2 (every layer)
-python train.py --model lenet --dataset mnist --l2_penalty "5e-4"
+python train_classificator.py --model lenet --dataset mnist --l2_penalty "5e-4"
 
 # LeNet: BN (every layer)
-python train.py --model lenet_bn --dataset mnist
+python train_classificator.py --model lenet_bn --dataset mnist
 
 # LeNet: dropout (every layer)
-python train.py --model lenet_dropout --dataset mnist
+python train_classificator.py --model lenet_dropout --dataset mnist
 
 # LeNet: binomial dropout (every layer)
-python train.py --model lenet_binomial_dropout --dataset mnist
+python train_classificator.py --model lenet_binomial_dropout --dataset mnist
 
 # LeNet: direct dropout (every layer)
-python train.py --model lenet_direct_dropout --dataset mnist
+python train_classificator.py --model lenet_direct_dropout --dataset mnist
 
 # LeNet: direct binomial dropout (every layer)
-python train.py --model lenet_direct_binomial_dropout --dataset mnist
+python train_classificator.py --model lenet_direct_binomial_dropout --dataset mnist
