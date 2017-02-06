03fd80af832d339e0d172d4bee97c2d8f42f3f5c
Paolo Galeone <nessuno@nerdz.eu>
2017-02-06 10:37:49 +0100

    inputs/utils.py: format

--- inputs/utils.py
+++ inputs/utils.py
@@ -165,11 +165,16 @@ def convert_to_tfrecords(dataset, name, data_dir):
     for index in range(num_examples):
         image_raw = images[index].tostring()
         example = tf.train.Example(features=tf.train.Features(feature={
-            'height': _int64_feature(rows),
-            'width': _int64_feature(cols),
-            'depth': _int64_feature(depth),
-            'label': _int64_feature(int(labels[index])),
-            'image_raw': _bytes_feature(image_raw)
+            'height':
+            _int64_feature(rows),
+            'width':
+            _int64_feature(cols),
+            'depth':
+            _int64_feature(depth),
+            'label':
+            _int64_feature(int(labels[index])),
+            'image_raw':
+            _bytes_feature(image_raw)
         }))
         writer.write(example.SerializeToString())
     writer.close()
