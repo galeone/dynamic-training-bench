f5f906f8e7d2d34c664575a94a34fd0858cb42b1
Paolo Galeone <nessuno@nerdz.eu>
2017-03-06 17:26:18 +0100

    scripts/evaluate.py: added. Evaluate from CLI

---
+++ scripts/evaluate.py
@@ -0,0 +1,33 @@
+#Copyright (C) 2017 Paolo Galeone <nessuno@nerdz.eu>
+#
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, you can obtain one at http://mozilla.org/MPL/2.0/.
+#Exhibit B is not attached; this software is compatible with the
+#licenses expressed under Section 1.12 of the MPL v2.
+""" Evaluate the model """
+
+import tensorflow as tf
+
+from sys import exit
+from dtb.evaluate import evaluate
+from dtb.inputs.interfaces import InputType
+
+from CLIArgs import CLIArgs
+
+
+def main():
+    """Evaluates the model, on the specified dataset,
+    fetching the requested input type"""
+    with tf.device(ARGS.eval_device):
+        print(
+            evaluate(ARGS.checkpoint_path, MODEL, DATASET, INPUT_TYPE,
+                     ARGS.batch_size))
+
+
+if __name__ == '__main__':
+    ARGS, MODEL, DATASET = CLIArgs(
+        description="Evaluate the model").parse_eval()
+
+    INPUT_TYPE = InputType.test if ARGS.test else InputType.validation
+    exit(main())
