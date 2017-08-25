49f6f73ad232aafc3cc96451d2cc9098d18dab29
Paolo Galeone <nessuno@nerdz.eu>
2017-08-25 18:14:41 +0200

    fix __init__. Copyed the wrong file

--- dytb/utils/__init__.py
+++ dytb/utils/__init__.py
@@ -5,12 +5,3 @@
 #file, you can obtain one at http://mozilla.org/MPL/2.0/.
 #Exhibit B is not attached; this software is compatible with the
 #licenses expressed under Section 1.12 of the MPL v2.
-"""Simplify the trainining and tuning of Tensorflow models"""
-
-from . import inputs
-from . import models
-
-__version__ = '0.6.4'
-__url__ = 'https://github.com/galeone/dynamic-training-bench'
-__author__ = 'Paolo Galeone'
-__email__ = 'nessuno@nerdz.eu'
