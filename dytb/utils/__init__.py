93507ed2fff04b4adca6be2df120cc7b09f0d7d2
Paolo Galeone <nessuno@nerdz.eu>
2017-08-25 18:07:03 +0200

    __init__.py -> dytb.utils

---
+++ dytb/utils/__init__.py
@@ -0,0 +1,16 @@
+#Copyright (C) 2017 Paolo Galeone <nessuno@nerdz.eu>
+#
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, you can obtain one at http://mozilla.org/MPL/2.0/.
+#Exhibit B is not attached; this software is compatible with the
+#licenses expressed under Section 1.12 of the MPL v2.
+"""Simplify the trainining and tuning of Tensorflow models"""
+
+from . import inputs
+from . import models
+
+__version__ = '0.6.4'
+__url__ = 'https://github.com/galeone/dynamic-training-bench'
+__author__ = 'Paolo Galeone'
+__email__ = 'nessuno@nerdz.eu'
