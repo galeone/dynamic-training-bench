f2ddeef04b121aaa943e70ef3fc748316243787c
Paolo Galeone <nessuno@nerdz.eu>
2018-02-28 16:19:59 +0100

    Fix #21

--- dytb/__init__.py
+++ dytb/__init__.py
@@ -10,7 +10,7 @@
 from . import inputs
 from . import models
 
-__version__ = '0.7.3'
+__version__ = '0.7.4'
 __url__ = 'https://github.com/galeone/dynamic-training-bench'
 __author__ = 'Paolo Galeone'
 __email__ = 'nessuno@nerdz.eu'
